40e9c0422e59d8f939033bb50cb1c568861e596c
Nofilbashir <nofil.bashiir@gmail.com>
2022-07-06 15:51:28 +0500

    latest push after changing avatar and name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import Waqar from '../data/waqar.jpeg';
+import avatar from '../data/avatar.jpg';
 // import Cart from './Cart';
 import Chat from './Chat';
 import Notification from './Notification';
@@ -68,20 +67,20 @@ const Navbar = () => {
           >
             <img
               className="rounded-full w-8 h-8"
-              src={Waqar}
+              src={avatar}
               alt="user-profile"
             />
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Waqar
+                Jhon Doe
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
 
-        {isClicked.cart && (<Cart />)}
+        {/* {isClicked.cart && (<Cart />)} */}
         {isClicked.chat && (<Chat />)}
         {isClicked.notification && (<Notification />)}
         {isClicked.userProfile && (<UserProfile />)}
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { Button } from '.';
 import { userProfileData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
-import waqar from '../data/waqar.jpeg';
+import avatar from '../data/avatar.jpg';
 
 const UserProfile = () => {
   const { currentColor } = useStateContext();
@@ -24,13 +24,13 @@ const UserProfile = () => {
       <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
         <img
           className="rounded-full h-24 w-24"
-          src={waqar}
+          src={avatar}
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Rai Waqar </p>
+          <p className="font-semibold text-xl dark:text-gray-200"> Jhon Doe </p>
           <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> wackar.afzal@gmail.com </p>
+          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> Jhon@email.com </p>
         </div>
       </div>
       <div>
